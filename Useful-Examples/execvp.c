9435b7f3cf0661e40e50309aaca93d48490633b5
Trevor Bakker <trevor.bakker@uta.edu>
2020-02-13 04:00:36 +0000

    Change to ls

--- Useful-Examples/execvp.c
+++ Useful-Examples/execvp.c
@@ -6,23 +6,21 @@
 int main( )
 {
   pid_t pid = fork( );
-
   if( pid == 0 )
   {
 
-    char **arguments;
+    char *arguments[4];
 
-    arguments[0] = ( char * ) malloc( strlen( "-a" ) );
+    arguments[0] = ( char * ) malloc( strlen( "ls" ) );
     arguments[1] = ( char * ) malloc( strlen( "-l" ) );
-    arguments[2] = ( char * ) malloc( strlen( "-t" ) );
 
-    strncpy( arguments[0], "-a", strlen( "-a" ) );
-    strncpy( arguments[1], "-l", strlen( "-l" ) );
-    strncpy( arguments[2], "-t", strlen( "-t" ) );
+    strncpy( arguments[0], "ls", strlen( "ls" ) );
+    strncpy( arguments[1], "-1", strlen( "-1" ) );
+
+    arguments[2] = NULL;
 
     // Notice you can add as many NULLs on the end as you want
-    int ret = execvp( "ls", arguments );  
-  
+    int ret = execvp( arguments[0], &arguments[0] );  
     if( ret == -1 )
     {
       perror("execl failed: ");
